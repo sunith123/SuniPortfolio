d55659d664a59dca2bd412b6421e244a41ecc620
Sunith Sukulan <49476919+sunith123@users.noreply.github.com>
2021-08-14 17:48:41 +0530

    Update subscriberform.php

--- forms/subscriberform.php
+++ forms/subscriberform.php
@@ -1,26 +1,20 @@
-<?php 
-if (isset($_POST['send_message'])) {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $msg = $_POST['msg'];
-  // Content-Type helps email client to parse file as HTML 
-  // therefore retaining styles
-  $headers = "MIME-Version: 1.0" . "\r\n";
-  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-  $message = "<html>
-  <head>
-  	<title>New message from website contact form</title>
-  </head>
-  <body>
-  	<h1>" . $subject . "</h1>
-  	<p>".$msg."</p>
-  </body>
-  </html>";
-  if (mail('sunithsukulan@gmail.com', $subject, $message, $headers)) {
-   echo "Email sent";
-  }else{
-   echo "Failed to send email. Please try again later";
-  }
-}
+<?php
+	if(isset($_POST['send_message'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$subject=$_POST['subject'];
+		$msg=$_POST['msg'];
+
+		$to='sunithsukulan@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Subject:".$subject."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
 ?>
